281e41da45b6664a6c8a6d41d19e16c4a32d04f4
Dominick Peluso <peluso.dominick@gmail.com>
2016-09-19 10:47:25 -0400

    Complete mocha setup

--- test/folder/test.spec.js
+++ test/folder/test.spec.js
@@ -1,5 +1,5 @@
 var chai = require('chai');
-var assert = chai.assert; 
+var assert = chai.assert;
 
 describe('Test', function() {
     describe('Testing', function() {
@@ -8,3 +8,4 @@ describe('Test', function() {
         });
     });
 });
+
