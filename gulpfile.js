2d485bcbe68a1d4a26bd86461522c9015833278d
Dominick Peluso <peluso.dominick@gmail.com>
2016-09-27 13:20:00 -0400

    Force mocha to quit on completeion

--- gulpfile.js
+++ gulpfile.js
@@ -45,10 +45,55 @@ var tsProject = ts.createProject(
     }
 );
 
+/**
+ * handleMochaError
+ * ================
+ * Simple error handling specifically for mocha. Reports the latest error
+ * encountered, then uses `process.exit(1)` to force an exit from the gulp
+ * task and communicate that an error occurred (e.g. to Travis CI).
+ */
+var mochaErr;
+var handleMochaError = function (err) {
+    console.log('Mocha encountered an error, exiting with status 1');
+    console.log('Error:', err.message);
+    process.exit(1);
+};
+
 gulp.task('test', function() {
     return gulp.src('test/**/*.spec.js', {read: false})
     // gulp-mocha needs filepaths so you can't have any plugins before it
         .pipe(mocha({reporter: 'nyan'}));
+});
+
+gulp.task('test-travis', function() {
+    return gulp.src('test/**/*.spec.js', {read: false})
+    // gulp-mocha needs filepaths so you can't have any plugins before it
+        .pipe(mocha({reporter: 'spec'}))
+        /**
+         * Keep track of latest error on Mocha. Because a failed test counts
+         * as an error, the process should not be exited until end of tests.
+         */
+        .on('error', function(err) {
+            /**
+             * This intermediate log is useful for when mocha crashes (as opposed
+             * to a test failing), especially necessary for Travis CI reporting.
+             * Without these logs, Travis CI will not report anything meaningful
+             * if mocha crashes. Can be commented out if desired.
+             */
+            console.error('ERROR:', err.message);
+            console.error('Stack:', err.stack);
+            mochaErr = err;
+        })
+        /**
+         * The methods below are a hack to get gulp to exit after mocha tests
+         * finish. Without them, `gulp mocha` doesn't exit and Travis
+         * never finishes running the tests.
+         */
+        .on('end', function () {
+            if (mochaErr) return handleMochaError(mochaErr);
+            // Force mocha to exit, because gulp-mocha is stupid.
+            process.exit();
+        });
 });
 
 gulp.task('build', function() {
@@ -64,6 +109,13 @@ gulp.task('build-test', function(callback) {
         'test',
         'tslint',
         // 'typedoc',
+        callback);
+});
+
+gulp.task('travis', function(callback) {
+    runSequence(
+        'build',
+        'test-travis',
         callback);
 });
 
