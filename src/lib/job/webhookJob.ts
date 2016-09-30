6a8dfa98a8223ddc6b9a0962e132466229178088
Dominick Peluso <peluso.dominick@gmail.com>
2016-09-30 11:44:15 -0400

    Add webhookjob helper methods

--- src/lib/job/webhookJob.ts
+++ src/lib/job/webhookJob.ts
@@ -1,11 +1,16 @@
 import {Environment} from "../environment/environment";
 import {Job} from "./job";
-import {ClientRequest} from "http";
+import {ServerRequest} from "http";
 import {ClientResponse} from "http";
+import {FileJob} from "./fileJob";
+
+const   fs = require("fs"),
+        tmp = require("tmp"),
+        url = require("url");
 
 export class WebhookJob extends Job {
 
-    protected request: ClientRequest;
+    protected request: ServerRequest;
     protected response: ClientResponse;
 
     /**
@@ -14,7 +19,7 @@ export class WebhookJob extends Job {
      * @param request
      * @param response
      */
-    constructor(e: Environment, request: ClientRequest, response: ClientResponse) {
+    constructor(e: Environment, request: ServerRequest, response: ClientResponse) {
         super(e, "Webhook Job");
         this.request = request;
         this.response = response;
@@ -30,10 +35,99 @@ export class WebhookJob extends Job {
 
     /**
      * Get the HTTP request object.
-     * @returns {any}
+     * @returns {ClientRequest}
      */
     public getRequest() {
         return this.request;
+    }
+
+    /**
+     * Return a specific URL parameter.
+     * #### Example
+     * ```js
+     * // Webhook URL: /hooks/my/hook?customer_id=MyCust
+     * var customer_id = webhookJob.getUrlParameter("customer_id");
+     * // customer_id => MyCust
+     * ```
+     * @param parameter
+     * @returns {any}
+     */
+    public getUrlParameter(parameter: string) {
+        let wh = this;
+        let url_parts = url.parse(wh.getRequest().url, true);
+        return url_parts.query[parameter];
+    }
+
+    /**
+     * Return all URl parameters.
+     * * #### Example
+     * ```js
+     * // Webhook URL: /hooks/my/hook?customer_id=MyCust&file_name=MyFile.zip
+     * var query = webhookJob.getUrlParameters();
+     * // query => {customer_id: "MyCust", file_name: "MyFile.zip"}
+     * ```
+     * @returns {any}
+     */
+    public getUrlParameters() {
+        let wh = this;
+        let url_parts = url.parse(wh.getRequest().url, true);
+        return url_parts.query;
+    }
+
+    /**
+     * Gets a FileJob from the request body with a temporary file name.
+     * The callback will be given the job as its parameter.
+     * #### Example
+     * ```js
+     *  webhookJob.getDataAsFileJob(function(fileJob){
+     *      fileJob.rename("myfile.zip");
+     *      fileJob.move(af.createFolderNest("/var/out/webhook"));
+     *  });
+     * ```
+     * @returns {any}
+     */
+    public getDataAsFileJob(callback: any) {
+        let wh = this;
+        let req = wh.getRequest();
+        let data = [];
+
+        req.on("data", function(chunk) {
+            data.push(chunk);
+        });
+        req.on("end", function() {
+            let buffer = Buffer.concat(data);
+
+            let filePath = tmp.tmpNameSync();
+            fs.writeFileSync(filePath, buffer);
+
+            let fileJob = new FileJob(wh.e, filePath);
+
+            callback(fileJob);
+        });
+    }
+
+    /**
+     * Get a string from the request body.
+     * The given callback is given a string parameter.
+     * #### Example
+     * ```js
+     * webhookJob.getDataAsString(function(requestBody){
+     *     console.log(requestBody);
+     * });
+     * ```
+     * @param callback
+     */
+    public getDataAsString(callback: any) {
+        let wh = this;
+        let req = wh.getRequest();
+        let data = "";
+
+        req.on("data", function(chunk) {
+            data += chunk;
+        });
+        req.on("end", function() {
+            callback(data);
+        });
     }
 
 }
