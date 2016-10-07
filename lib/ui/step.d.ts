6612797903793305e93aefd8826cd488577b2636
Dominick Peluso <peluso.dominick@gmail.com>
2016-10-07 11:03:58 -0400

    Export interface

---
+++ lib/ui/step.d.ts
@@ -0,0 +1,17 @@
+/**
+ * An interface step which allows GET requests to be made against the interface itself.
+ */
+export interface Step {
+    /**
+     * The human-readable step name.
+     */
+    name: string;
+    /**
+     * Flag if step is complete or not
+     */
+    complete?: boolean;
+    /**
+     * Callback function to run on step execution.
+     */
+    callback?: any;
+}
