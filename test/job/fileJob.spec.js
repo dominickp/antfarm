2916e642107814dec4c90b31243077d95e765953
Dominick Peluso <peluso.dominick@gmail.com>
2016-11-04 17:23:07 -0400

    Use correct env location

--- test/job/fileJob.spec.js
+++ test/job/fileJob.spec.js
@@ -22,7 +22,7 @@ describe('FileJob', function() {
 
     afterEach("remove temporary file", function(done){
         tempFolderCleanupCallback();
-        if(process.NODE_ENV === "TEST") {
+        if(process.env.NODE_ENV === "TEST") {
             setTimeout(done, 500);
         } else {
             done();
